d303460041d92b9cbe1f8bf4d79f7bd2451f6400
jeecms <randy_shandong@163.com>
2018-01-09 23:12:59 +0800

    category

---
+++ src/main/java/com/hellojd/shopex/common/Order.java
@@ -0,0 +1,85 @@
+package com.hellojd.shopex.common;
+
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import java.io.Serializable;
+
+public class Order
+        implements Serializable
+{
+    private static final long serialVersionUID = -3078342809727773232L;
+    private static final Order.Direction DESC = Order.Direction.desc;
+    private String property;
+    private Order.Direction direction = DESC;
+
+    public Order()
+    {
+    }
+
+    public Order(String property, Order.Direction direction)
+    {
+        this.property = property;
+        this.direction = direction;
+    }
+
+    public static Order asc(String property)
+    {
+        return new Order(property, Order.Direction.asc);
+    }
+
+    public static Order desc(String property)
+    {
+        return new Order(property, Order.Direction.desc);
+    }
+
+    public String getProperty()
+    {
+        return this.property;
+    }
+
+    public void setProperty(String property)
+    {
+        this.property = property;
+    }
+
+    public Order.Direction getDirection()
+    {
+        return this.direction;
+    }
+
+    public void setDirection(Order.Direction direction)
+    {
+        this.direction = direction;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        if (this == obj)
+            return true;
+        Order localOrder = (Order)obj;
+        return new EqualsBuilder().append(getProperty(), localOrder.getProperty()).append(getDirection(), localOrder.getDirection()).isEquals();
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return new HashCodeBuilder(17, 37).append(getProperty()).append(getDirection()).toHashCode();
+    }
+
+    public enum Direction
+    {
+        asc, desc;
+
+        public static Direction fromString(String value)
+        {
+            return valueOf(value.toLowerCase());
+        }
+    }
+}
