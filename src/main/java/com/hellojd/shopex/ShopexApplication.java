a00a9687e5ca0bf9318b9fbce4453805ccc3e601
jeecms <randy_shandong@163.com>
2018-01-11 22:37:19 +0800

    jetty ctrl + shift + F9

--- src/main/java/com/hellojd/shopex/ShopexApplication.java
+++ src/main/java/com/hellojd/shopex/ShopexApplication.java
@@ -2,11 +2,20 @@ package com.hellojd.shopex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ShopexApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShopexApplication.class, args);
+	}
+	@Bean
+	public EmbeddedServletContainerFactory servletContainer() {
+		JettyEmbeddedServletContainerFactory factory =
+				new JettyEmbeddedServletContainerFactory();
+		return factory;
 	}
 }
